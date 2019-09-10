01dcbb1283acb0d547e39ed22302102bb1e7df86
Rafael Kunz <rafakunz@gmail.com>
2019-09-10 01:28:59 -0300

    Html atualizado 2

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,20 +1,51 @@
-import Vue from 'vue'
+
 import axios from 'axios'
-import VueAxios from 'vue-axios'
  
-Vue.use(VueAxios, axios)
 
 new Vue({
     el: '#app',
-    data () {
-      return {
-        info: null
-      }
+    data: {
+      cards: []
     },
     mounted () {
-      axios
-        .get('https://pmweb-dev.github.io/resumeCards.json')
-        .then(response => (this.info = response.cards.hours_music))
-        //.then(response => (this.info = response.cards.quantity_bands))
+      axios.get('https://pmweb-dev.github.io/resumeCards.json')
+        .then(response => (this.cards = response.hours_music.hours))
+        .then(response => (this.cards = response.hours_music.percent_diference))
     }
   })
+
+  console.log()
+
+  // {
+  //   "cards":{
+  //     "hours_music":{
+  //       "hours": 235,
+  //       "percent_diference": 130
+  //     },
+  //     "quantity_bands":{
+  //       "bands": 48,
+  //       "percent_diference": 98
+  //     },
+  //     "lost_artists":{
+  //       "losts": 12,
+  //       "percent_diference": 37
+  //     }
+  //   }
+  // }
+
+// export default {
+//   name: 'app',
+//   data () {
+//     return {
+//       info: null
+//     }
+//   },
+//   created() {
+// axios.get('https://pmweb-dev.github.io/resumeCards.json').then((response) => {
+//       this.info = response.cards.hours_music
+//     })
+//     .catch((e) => {
+//       console.error(e)
+//     })
+//   }
+// }
